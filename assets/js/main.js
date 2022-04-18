0532c80ddf63613b94cfc0a8b70114948524c414
raulultul <102937815+raulultul@users.noreply.github.com>
2022-04-19 00:48:43 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,232 @@
+/**
+main js
+*/
+(function($) {
+  "use strict";
+
+  // Preloader
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  var nav = $('nav');
+  var navHeight = nav.outerHeight();
+
+  /*--/ ScrollReveal /Easy scroll animations for web and mobile browsers /--*/
+  window.sr = ScrollReveal();
+  sr.reveal('.foo', {
+    duration: 1000,
+    delay: 15
+  });
+
+  /*--/ Carousel owl /--*/
+  $('#carousel').owlCarousel({
+    loop: true,
+    margin: -1,
+    items: 1,
+    nav: true,
+    navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
+  });
+
+  /*--/ Animate Carousel /--*/
+  $('.intro-carousel').on('translate.owl.carousel', function() {
+    $('.intro-content .intro-title').removeClass('animate__zoomIn animate__animated').hide();
+    $('.intro-content .intro-price').removeClass('animate__fadeInUp animate__animated').hide();
+    $('.intro-content .intro-title-top, .intro-content .spacial').removeClass('animate__fadeIn animate__animated').hide();
+  });
+
+  $('.intro-carousel').on('translated.owl.carousel', function() {
+    $('.intro-content .intro-title').addClass('animate__zoomIn animate__animated').show();
+    $('.intro-content .intro-price').addClass('animate__fadeInUp animate__animated').show();
+    $('.intro-content .intro-title-top, .intro-content .spacial').addClass('animate__fadeIn animate__animated').show();
+  });
+
+  /*--/ Navbar Collapse /--*/
+  $('.navbar-toggle-box-collapse').on('click', function() {
+    $('body').removeClass('box-collapse-closed').addClass('box-collapse-open');
+  });
+  $('.close-box-collapse, .click-closed').on('click', function() {
+    $('body').removeClass('box-collapse-open').addClass('box-collapse-closed');
+    $('.menu-list ul').slideUp(700);
+  });
+
+  /*--/ Navbar Menu Reduce /--*/
+  $(window).trigger('scroll');
+  $(window).bind('scroll', function() {
+    var pixels = 50;
+    var top = 1200;
+    if ($(window).scrollTop() > pixels) {
+      $('.navbar-default').addClass('navbar-reduce');
+      $('.navbar-default').removeClass('navbar-trans');
+    } else {
+      $('.navbar-default').addClass('navbar-trans');
+      $('.navbar-default').removeClass('navbar-reduce');
+    }
+    if ($(window).scrollTop() > top) {
+      $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+    } else {
+      $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+    }
+  });
+
+  /*--/ Property owl /--*/
+  $('#property-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      769: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      }
+    }
+  });
+
+  /*--/ Property owl owl /--*/
+  $('#property-single-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
+    responsive: {
+      0: {
+        items: 1,
+      }
+    }
+  });
+
+  /*--/ News owl /--*/
+  $('#new-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      769: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      }
+    }
+  });
+
+  /*--/ Testimonials owl /--*/
+  $('#testimonial-carousel').owlCarousel({
+    margin: 0,
+    autoplay: true,
+    nav: true,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeInUp',
+    navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      }
+    }
+  });
+
+})(jQuery);
+///me
+ 
+var masonry = new Macy({
+  container: '#macy',
+  trueOrder: false,
+  waitForImages: false,
+  useOwnImageLoader: false,
+  debug: true,
+  mobileFirst: true,
+  columns: 1,
+  margin: {
+      y: 16,
+      x: '2%',
+  },
+  breakAt: {
+      1200: 6,
+      940: 5,
+      520: 3,
+      400: 2
+  },
+});
+
+
+//mixtup
+
+
+var containerEl = document.querySelector('.gallaxy');
+
+var mixer = mixitup (containerEl,{
+    selectors:{
+       control:'[our-mix-control]'
+    }
+})
+///owl carousel
+jQuery(document).ready(function($) {
+  $('.loop').owlCarousel({
+    center: true,
+    items: 2,
+    loop: true,
+    margin: 10,
+    responsive: {
+      600: {
+        items: 3
+      }
+    }
+  });
+  $('.nonloop').owlCarousel({
+    center: true,
+    items: 2,
+    loop: false,
+    margin: 5,
+    responsive: {
+      600: {
+        items: 3
+      }
+    }
+  });
+});
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    margin: 10,
+    autoHeight: true
+  });
+})
+
+
+
+
+
+
+
+
+
